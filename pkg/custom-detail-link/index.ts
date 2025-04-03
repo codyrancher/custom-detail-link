b93705eddc77a93e6888d3261ad1108a5909a8fe
Cody Jackson <cody.jackson@suse.com>
2025-04-03 10:23:15 -0700

    first

---
+++ pkg/custom-detail-link/index.ts
@@ -0,0 +1,36 @@
+import { importTypes } from '@rancher/auto-import';
+import { IPlugin, TableColumnLocation } from '@shell/core/types';
+
+// Init the package
+export default function (plugin: IPlugin): void {
+  // Auto-import model, detail, edit from the folders
+  importTypes(plugin);
+
+  // Provide plugin metadata from package.json
+  plugin.metadata = require('./package.json');
+
+  plugin.addTableColumn(
+    TableColumnLocation.RESOURCE,
+    { resource: ['configmap'] },
+    {
+      name: 'some-prop-col',
+      label: 'Custom Detail Link',
+      getValue: (row: any) => {
+        return 'Custom Detail Link'
+      },
+      formatter: 'LinkDetail',
+      formatterOpts: {
+        getCustomDetailLink: (row) => {
+          console.log('this is called');
+
+          return {
+            name: 'home'
+          };
+        },
+      },
+    }
+  )
+
+  // Load a product
+  // plugin.addProduct(require('./product'));
+}
